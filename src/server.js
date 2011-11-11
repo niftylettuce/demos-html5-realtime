1eeb22bde14f0340bdaeedb1e4a2c09095eaa150
Nick Baugh <niftylettuce@gmail.com>
2011-11-10 20:41:45 -0500

    test

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var express = require('express')
 
 app.use(express.static(__dirname + '/www'));
 
-app.listen(2000);
+app.listen(3030);
 
 io.sockets.on('connection', function(socket) {
   console.log("Connected");
